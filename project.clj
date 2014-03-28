cdf06ab0df5917326c42c1bd0070ebe02ab193a1
Bruno Vecchi <vecchi.b@gmail.com>
2014-03-28 00:10:00 -0300

    Fix clojure jar dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ororo "0.1.0"
   :description "A Wunderground API library."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.1.2"]
                  [clj-http "0.2.1"]
                  [http-kit "2.1.18"]])
