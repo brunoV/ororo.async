e03daa720c3ba963b4399bede48f9733fab338f8
Bruno Vecchi <vecchi.b@gmail.com>
2014-06-09 11:34:19 -0300

    Update clojure, remove old http library dep

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "A Wunderground API library."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.4"]
-                 [clj-http "0.2.1"]
                  [http-kit "2.1.18"]])
