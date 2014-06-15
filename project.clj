3e23108c5fdd21850829763b9837f1c61eea498b
Bruno Vecchi <vecchi.b@gmail.com>
2014-06-15 17:08:14 -0300

    Add metadata to project.clj
    Remove license file, redundant with :license field in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject ororo.async "0.1.0"
   :description "An asynchronous Wunderground API library."
+  :url "https://github.com/brunoV/ororo.async"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.4"]
                  [http-kit "2.1.18"]])
